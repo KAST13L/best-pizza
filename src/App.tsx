a0fea9c65abfb97fc5a367514cc877bdfb948b66
KAST13L <o4enbsmeshno@gmail.com>
2022-12-08 11:48:51 +0200

    feat: added eslint and prettier in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export const App = () => {
-
-    return (
-        <div>
-          <h1>You are welcome!</h1>
-        </div>
-    );
+export const App = (): any => {
+  return (
+    <div>
+      <h1>You are welcome!</h1>
+    </div>
+  );
 };
